9b5cea68162f8f0658369047903ea0bc9fdd257b
dcharubini <dcharubini@gmail>
2012-07-19 17:48:48 -0400

    update readme

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject rbf "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "First attempt at writing some RBF code"
   :dependencies [[org.clojure/clojure "1.4.0"]
 		 [org.clojure/math.numeric-tower "0.0.1"]
 		 [incanter "1.3.0"]])
