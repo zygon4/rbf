3e6d4c8dd8903466f220fccf1fd1d70bedf2c604
dcharubini <dcharubini@gmail>
2012-07-19 17:13:34 -0400

    first

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject rbf "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+		 [org.clojure/math.numeric-tower "0.0.1"]
+		 [incanter "1.3.0"]])
